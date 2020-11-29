cbb0a8813d335199e99d3d8b3e5f9a4d68d8867e
FaiNa-O3 <73303034+FaiNa-O3@users.noreply.github.com>
2020-11-29 22:11:28 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <!-- fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+    
+    <!-- my CSS -->
+    <link rel="stylesheet" href="style.css">
+
+    <title>Home Page</title>
+  </head>
+  <body>
+    <!-- navbar -->
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container">
+          <a class="navbar-brand" href="https://soluteknusantara.com/">Solutek</a>
+          
+            
+          
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ml-auto">
+              <a class="nav-item nav-link active" href="https://soluteknusantara.com/">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-item nav-link" href="#">Paket</a>
+              <a class="nav-item nav-link" href="#">Project</a>
+              <a class="nav-item nav-link " href="#" tabindex="-1" aria-disabled="true">kontak</a>
+              <!-- //<a class="nav-item btn btn-primary tombol" href="#" tabindex="-1" aria-disabled="true">lolol</a> -->
+            </div>
+          </div>
+        </div>
+      </nav>
+    <!-- end navbar -->
+
+    <!-- jumbotron -->
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Get work done <span>faster</span><br> and <span>better</span> with us </h1>
+        <a href="" class="btn btn-primary tombol">Our Work</a>
+        
+      </div>
+    </div>
+    <!-- end jumbotron -->
+
+      <!-- container -->
+      <div class="container">
+        <!-- info panel -->
+        <div class="row justify-content-center">
+          <div class="col-10 info-panel">
+            <div class="row">
+              <div class="col-lg">
+                <img src="img/employee.png" alt="icon1" class="float-left">
+                <h4>24 hours</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div class="col-lg">
+                <img src="img/hires.png" alt="icon2" class="float-left">
+                <h4>High-Res</h4>
+                <p>Lorem ipsum dolor sit amet.</p>
+              </div>
+              <div class="col-lg">
+                <img src="img/security.png" alt="icon3" class="float-left">
+                <h4>security</h4>
+                <p>Lorem ipsum dolor sit amet.sdggsdgdfgg srdgdf df gdf df gdf d sgfdg</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- end info -->
+
+        <!-- space -->
+        <div class="row space">
+          <div class="col-lg-6">
+            <img src="img/workingspace.jpg" alt="workingspace" class="img-fluid">
+          </div>
+          <div class="col-lg-5">
+            <h3>membuat <span>website</span> dengan <span>mudah</span></h3>
+            <p>Sebagai jasa pembuatan website, Solusi Teknologi Nusantara adalah partner terbaik untuk bisnis Anda dalam menghadapi Era Digital sekarang ini. Jadikan bisnis Anda sebagai bisnis modern yang mudah dijangkau semua orang di Indonesia.</p>
+            <a href=" " class="btn btn-primary tombol">Coba Sekarang!</a>
+          </div>
+        </div>
+        <!-- end space -->
+
+      </div>
+      <!-- end container -->
+
+      <!-- footer -->
+        <section class=" footer footer-fluid">
+          <div class="container">
+            <h5>Solusi Teknologi Nusantara</h5>
+          </div>
+          <div class="min-footer">
+            <p>Copyright © PT Solusi Teknologi Nusantara.  All rights reserved													</p>
+          </div>
+        </section>
+        <!-- end footer -->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
